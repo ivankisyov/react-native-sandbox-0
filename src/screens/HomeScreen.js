5b68019925a6f3890176e15b2f6c5c84a16426b5
Ivan Kisyov <dev.vanks@gmail.com>
2023-12-26 15:21:15 +0200

    Progress: - Intro to navigation - Button - TouchableOpacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   const greeting2 = "Hi there!";
-  const greeting3 = <Text style={styles.text}>{greeting2}</Text>;
 
   return (
     <View>
       <Text style={styles.text}>HomeScreen 1</Text>
-      <Text style={styles.text}>{greeting2}</Text>
-      {greeting3}
+      <Button
+        title="Go to components demo"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go to list demo"
+        onPress={() => navigation.navigate("ListScreen")}
+      />
+      {/* <TouchableOpacity onPress={() => navigation.navigate("ListScreen")}>
+        <Text>Go to list demo</Text>
+      </TouchableOpacity> */}
     </View>
   );
 };
